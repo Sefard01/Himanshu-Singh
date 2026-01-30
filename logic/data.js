fdb176d01eb8d084efd79dca76fb7756a0c956a4
Himanshu <himanshusingh9783@gmail.com>
2026-01-30 16:38:41 +0530

    add poems data

---
+++ logic/data.js
@@ -0,0 +1,79 @@
+const DATA = {
+  books: [
+    {
+      title: "The Human Cloud",
+      img: "assets/the_human_cloud.jpg",
+      link: "The_human_cloud[1].pdf"
+    },
+    {
+      title: "मैं सोचता हूँ कि मैं सोचता हूँ",
+      img: "assets/mainSochtaHunKiMainSochtaHun.png",
+      link: "Main Sochta Hoon Ki Main Sochta Hoon.pdf"
+    }
+  ],
+
+  poems: [
+    {
+      title: "Result fir se.",
+      link: "https://www.instagram.com/reel/DTiKZ7cCLg2/?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+
+    },
+
+    {
+      title: "intjar fir se.",
+      link: "https://www.instagram.com/reel/DTZcsHekQS0/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    },
+    {
+      title: "Koi nhi rahta",
+      link: "https://www.instagram.com/reel/DTZaaBOEZIG/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    },
+    {
+      title: "Nashe me hai",
+      link: "https://www.instagram.com/reel/DTHnqnqkQON/?utm_source=ig_web_copy_link"
+    },
+    {
+      title: "Samaj hi sangharsh hai.",
+      link: "https://www.instagram.com/reel/DTFzVhqiNQF/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    },
+    {
+      title: "Maine seekh liya.",
+      link: "https://www.instagram.com/reel/DS6jc3NESnQ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    },
+    {
+      title: "Main kyu padhta hun.",
+      link: "https://www.instagram.com/reel/DS384hRkzoA/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    },
+    {
+      title: "Mere vichar.",
+      link: "https://www.instagram.com/reel/DS1YI8JkxY1/?utm_source=ig_web_copy_link"
+    },
+    {
+      title: "Mein likhta hu.",
+      link: "https://www.instagram.com/reel/DSvEaB6Esyq/?utm_source=ig_web_copy_link"
+    },
+    {
+      title: "main bhool gya.",
+      link: "https://www.instagram.com/p/DSp_Nxckrey/"
+    },
+    {
+      title: "Tum samajh paate.",
+      link: "https://www.instagram.com/reel/DSoVnHnEa01/"
+    }
+
+  ],
+
+  projects: [
+    {
+      name: "Music Player",
+      link: "https://sefard01.github.io/Music-player-/"
+    },
+    {
+      name: "Hot Or Not",
+      link: "https://sefard01.github.io/Hot-Or-Not-website-/"
+    },
+    {
+      name: "Sleepy Cursor",
+      link: "https://sefard01.github.io/Sleepy-cursor-/"
+    }
+  ]
+};
